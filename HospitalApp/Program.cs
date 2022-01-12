8fb9afb34618661f5e2370640975bc9ceee1f094
Lykor <gradbit1@gmail.com>
2022-01-13 03:37:27 +0500

    Update Program.cs

--- HospitalApp/Program.cs
+++ HospitalApp/Program.cs
@@ -1,1 +1,16 @@
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace HospitalApp
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
