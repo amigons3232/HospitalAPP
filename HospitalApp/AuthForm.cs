8cbebc6a1d0f35bdd5437336ac35ae2c3a1d1d1b
Georgii <51374771+amigons3232@users.noreply.github.com>
2022-01-13 04:45:25 +0600

    Add files via upload

--- HospitalApp/AuthForm.cs
+++ HospitalApp/AuthForm.cs
@@ -1,1 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
 
+namespace HospitalApp
+{
+    public partial class AuthForm : Form
+    {
+        public AuthForm()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == Properties.Settings.Default.Логин && maskedTextBox1.Text == Properties.Settings.Default.Пароль)
+            {
+                MainForm mainForm = new MainForm(this);
+                mainForm.Show();
+                this.Hide();
+            }
+        }
+    }
+}
