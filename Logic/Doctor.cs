474659c6710ad6aca99688787ec014f2550962da
garyyyskz <91424551+garyyyskz@users.noreply.github.com>
2022-01-13 10:53:30 +0600

    Update Doctor.cs

--- Logic/Doctor.cs
+++ Logic/Doctor.cs
@@ -6,24 +6,5 @@ using System.Threading.Tasks;
 
 namespace Logic
 {
-    public class Doctor
-    {
-        string id;
-        string fio;
-        string spec;
-        public Doctor(string fio,string spec)
-        {
-            this.fio = fio;
-            this.spec = spec;
-        }
-        public Doctor(string id, string fio, string spec)
-        {
-            this.id = id;
-            this.fio = fio;
-            this.spec = spec;
-        }
-        public string Id { get => id; set => id = value; }
-        public string Fio { get => fio; set => fio = value; }
-        public string SPec { get => spec; set => spec = value; }
-    }
+    
 }
