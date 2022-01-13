af8ddcaa0039949b4fc97d58fbc70d15b1231f20
garyyyskz <91424551+garyyyskz@users.noreply.github.com>
2022-01-13 10:50:38 +0600

    Update DoctorFabric.cs

--- LogicHospital/DoctorFabric.cs
+++ LogicHospital/DoctorFabric.cs
@@ -22,4 +22,19 @@ namespace LogicHospital
             throw new NotImplementedException();
         }
 
-        
+        public override Palata createPalata(string num, string size, string otdel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Patient createPatient(string fio, string idPalata, string idDoctor, string DateIn, string diagnoz, string DateOut)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Patient createPatient(string id, string fio, string idPalata, string idDoctor, string DateIn, string diagnoz, string DateOut)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
