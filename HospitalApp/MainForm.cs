f42e581b6f25e2a36d0b04d600fbd8f4f0f7e3de
garyyyskz <91424551+garyyyskz@users.noreply.github.com>
2022-01-13 11:17:58 +0600

    Update MainForm.cs

--- HospitalApp/MainForm.cs
+++ HospitalApp/MainForm.cs
@@ -293,14 +293,6 @@ namespace HospitalApp
             }
         }
 
-        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            exportToExcel(dataGridView1);
-        }
-
-        private void справкмToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            Process.Start("Справка.pdf");
-        }
+        
     }
 }
