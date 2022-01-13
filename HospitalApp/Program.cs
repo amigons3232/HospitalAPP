dfa8230152fa44ef51730ff0624c80964e416c9b
garyyyskz <91424551+garyyyskz@users.noreply.github.com>
2022-01-13 11:20:49 +0600

    Update Program.cs

--- HospitalApp/Program.cs
+++ HospitalApp/Program.cs
@@ -3,19 +3,4 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 
-namespace HospitalApp
-{
-    static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new AuthForm());
-        }
-    }
-}
+
