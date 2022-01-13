5a1390a602fb49db1c0c07600e89ad511df87db4
garyyyskz <91424551+garyyyskz@users.noreply.github.com>
2022-01-13 11:14:51 +0600

    Update DoctorsForm.cs

--- HospitalApp/DoctorsForm.cs
+++ HospitalApp/DoctorsForm.cs
@@ -28,27 +28,7 @@ namespace HospitalApp
             врачиTableAdapter.Fill(dbDataSet.Врачи);
         }
 
-        private void DoctorsForm_Load(object sender, EventArgs e)
-        {
-            // TODO: This line of code loads data into the 'dbDataSet.Врачи' table. You can move, or remove it, as needed.
-            this.врачиTableAdapter.Fill(this.dbDataSet.Врачи);
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            if (dataGridView1.SelectedRows.Count > 0)
-            {
-                foreach (Control c in editPanel.Controls)
-                {
-                    foreach (Binding b in c.DataBindings)
-                    {
-                        b.WriteValue();
-                    }
-                }
-                saveChanges();
-            }
-        }
+        
 
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
