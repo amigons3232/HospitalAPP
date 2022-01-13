fe8f89bb470fd1b15cc26c21fe38b05787953008
garyyyskz <91424551+garyyyskz@users.noreply.github.com>
2022-01-13 11:16:04 +0600

    Update Palata.cs

--- LogicHospital/Palata.cs
+++ LogicHospital/Palata.cs
@@ -3,35 +3,4 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace LogicHospital
-{
- 
-    public class Palata
-    {
-        string id;
-        string num;
-        string size;
-        string otdel;
 
-        public Palata(string id, string num, string size, string otdel)
-        {
-            this.id = id;
-            this.num = num;
-            this.size = size;
-            this.otdel = otdel;
-        }
-
-        public Palata(string num, string size, string otdel)
-        {
-            this.num = num;
-            this.size = size;
-            this.otdel = otdel;
-        }
-
-        public string Id { get => id; set => id = value; }
-        public string Num { get => num; set => num = value; }
-           
-        public string Size { get => size; set => size = value; }
-        public string Otdel { get => otdel; set => otdel = value;}
-    }
-}
