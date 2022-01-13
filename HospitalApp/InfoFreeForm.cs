17142a1fd79e0a5abd9284a457a21a482e702c9d
garyyyskz <91424551+garyyyskz@users.noreply.github.com>
2022-01-13 11:22:15 +0600

    Update InfoFreeForm.cs

--- HospitalApp/InfoFreeForm.cs
+++ HospitalApp/InfoFreeForm.cs
@@ -12,16 +12,7 @@ namespace HospitalApp
 {
     public partial class InfoFreeForm : Form
     {
-        Form parentForm;
-        public InfoFreeForm(Form parentForm)
-        {
-            InitializeComponent();
-            this.parentForm = parentForm;
-            // TODO: данная строка кода позволяет загрузить данные в таблицу "dbDataSet.Пациенты". При необходимости она может быть перемещена или удалена.
-            this.пациентыTableAdapter.Fill(this.dbDataSet.Пациенты);
-            // TODO: данная строка кода позволяет загрузить данные в таблицу "dbDataSet.Палаты". При необходимости она может быть перемещена или удалена.
-            this.палатыTableAdapter.Fill(this.dbDataSet.Палаты);
-        }
+        
 
         private void InfoFreeForm_Load(object sender, EventArgs e)
         {
